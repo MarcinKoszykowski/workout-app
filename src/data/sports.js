9159e46dacd3d7121161e02d98f5b2a88da0b310
Marcin Koszykowski <m.koszykowski92@gmail.com>
2020-03-25 10:46:48 +0100

    Add new sports and kcal in data/sports

--- src/data/sports.js
+++ src/data/sports.js
@@ -1,96 +1,157 @@
+import badmintonIcon from 'assets/icons/sports/badminton.svg';
 import basketballIcon from 'assets/icons/sports/basketball.svg';
+import billiardIcon from 'assets/icons/sports/billiard.svg';
+import bowlingIcon from 'assets/icons/sports/bowling.svg';
 import boxIcon from 'assets/icons/sports/box.svg';
 import cardioIcon from 'assets/icons/sports/cardio.svg';
 import cyclingIcon from 'assets/icons/sports/cycling.svg';
 import dumbbellIcon from 'assets/icons/sports/dumbbell.svg';
 import footballIcon from 'assets/icons/sports/football.svg';
+import golfIcon from 'assets/icons/sports/golf.svg';
 import hockeyIcon from 'assets/icons/sports/hockey.svg';
 import runningIcon from 'assets/icons/sports/running.svg';
 import skatingIcon from 'assets/icons/sports/skating.svg';
 import swimmingIcon from 'assets/icons/sports/swimming.svg';
+import tableTennisIcon from 'assets/icons/sports/table-tennis.svg';
 import tennisIcon from 'assets/icons/sports/tennis.svg';
 import volleyballIcon from 'assets/icons/sports/volleyball.svg';
 import zumbaIcon from 'assets/icons/sports/zumba.svg';
 
+import badmintonImage from 'assets/images/sports/badminton.jpg';
 import basketballImage from 'assets/images/sports/basketball.jpg';
+import billiardImage from 'assets/images/sports/billiard.jpg';
+import bowlingImage from 'assets/images/sports/bowling.jpg';
 import boxImage from 'assets/images/sports/box.jpg';
 import cardioImage from 'assets/images/sports/cardio.jpg';
 import cyclingImage from 'assets/images/sports/cycling.jpg';
 import dumbbellImage from 'assets/images/sports/dumbbell.jpg';
 import footballImage from 'assets/images/sports/football.jpg';
+import golfImage from 'assets/images/sports/golf.jpg';
 import hockeyImage from 'assets/images/sports/hockey.jpg';
 import runningImage from 'assets/images/sports/running.jpg';
 import skatingImage from 'assets/images/sports/skating.jpg';
 import swimmingImage from 'assets/images/sports/swimming.jpg';
+import tableTennisImage from 'assets/images/sports/table-tennis.jpg';
 import tennisImage from 'assets/images/sports/tennis.jpg';
 import volleyballImage from 'assets/images/sports/volleyball.jpg';
 import zumbaImage from 'assets/images/sports/zumba.jpg';
 
 const sports = [
   {
+    name: 'badminton',
+    icon: badmintonIcon,
+    background: badmintonImage,
+    kcal: 5.7,
+  },
+  {
     name: 'basketball',
     icon: basketballIcon,
     background: basketballImage,
+    kcal: 8,
+  },
+  {
+    name: 'billiard',
+    icon: billiardIcon,
+    background: billiardImage,
+    kcal: 2.4,
+  },
+  {
+    name: 'bowling',
+    icon: bowlingIcon,
+    background: bowlingImage,
+    kcal: 3,
   },
   {
     name: 'box',
     icon: boxIcon,
     background: boxImage,
+    kcal: 9.3,
   },
   {
     name: 'cardio',
     icon: cardioIcon,
     background: cardioImage,
+    kcal: 7,
+    low: 0.7,
+    high: 1.3,
   },
   {
     name: 'cycling',
     icon: cyclingIcon,
     background: cyclingImage,
+    kcal: 6.8,
+    low: 0.6,
+    high: 1.4,
   },
   {
     name: 'dumbbell',
     icon: dumbbellIcon,
     background: dumbbellImage,
+    kcal: 6,
   },
   {
     name: 'football',
     icon: footballIcon,
     background: footballImage,
+    kcal: 9,
+  },
+  {
+    name: 'golf',
+    icon: golfIcon,
+    background: golfImage,
+    kcal: 4.5,
   },
   {
     name: 'hockey',
     icon: hockeyIcon,
     background: hockeyImage,
+    kcal: 8.5,
   },
   {
     name: 'running',
     icon: runningIcon,
     background: runningImage,
+    kcal: 10.5,
+    low: 0.75,
+    high: 1.25,
   },
   {
     name: 'skating',
     icon: skatingIcon,
     background: skatingImage,
+    kcal: 7.3,
   },
   {
     name: 'swimming',
     icon: swimmingIcon,
     background: swimmingImage,
+    kcal: 8.3,
+    low: 0.8,
+    high: 1.2,
+  },
+  {
+    name: 'table-tennis',
+    icon: tableTennisIcon,
+    background: tableTennisImage,
+    kcal: 4,
   },
   {
     name: 'tennis',
     icon: tennisIcon,
     background: tennisImage,
+    kcal: 8,
   },
   {
     name: 'volleyball',
     icon: volleyballIcon,
     background: volleyballImage,
+    kcal: 6,
   },
   {
     name: 'zumba',
     icon: zumbaIcon,
     background: zumbaImage,
+    kcal: 6.8,
   },
 ];
 
